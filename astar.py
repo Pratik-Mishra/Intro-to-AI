3becb22df41b7b29aaa29f8733f2f99b1ef4c396
Pratik-Mishra <48774881+Pratik-Mishra@users.noreply.github.com>
2019-07-14 19:51:22 -0400

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,14 +1,15 @@
+import time
 import random
 import math
 
 class Node:
     
-    def __init__(self,value,point):
+    def __init__(self,value,point, parent, g, h):
         self.value = value
         self.point = point
-        self.parent = None
-        self.g = 0
-        self.h = 0
+        self.parent = parent
+        self.g = g
+        self.h = h
         
     def cost(self,other):
         return 0 if self.value == 1 else 1
@@ -26,24 +27,29 @@ def grid(dim, p):
                 maze[i][j] = 0
             else:
                 maze[i][j] = block(p)
-    for row in maze:
-        print(' '.join([str(elem) for elem in row]))
+##    for row in maze:
+##        print(' '.join([str(elem) for elem in row]))
     return maze
 
 def children(curr, maze):
     x, y = curr.point
-    links = [grid[d[0]][d[1]] for d in [(x-1, y),(x,y - 1),(x,y + 1),(x+1,y)]]
-    return [link for link in links if link.value != '%']
+    links = []
+    for d in [[0, -1], [1, 0], [0, 1], [-1, 0]]:
+            neighbor = [curr.point[0]+d[0], curr.point[1]+d[1]]
+            if 0 <= neighbor[0] <= len(maze)-1 and 0 <= neighbor[1] <= len(maze)-1:
+                if maze[neighbor[0]][neighbor[1]].value == 0:
+                    links.append(maze[neighbor[0]][neighbor[1]])
+    return links
 
 def aStar(start, goal, maze, heuristic):
     openset = []
     closedset = []
     curr = start
     openset.append(curr)
+    path = []
     while openset:
-        curr = min(openset, key = lambda o:o.g + o.h)
-        if curr == goal:
-            path = []
+        curr = min(openset, key = lambda f:f.g + f.h)
+        if curr.point == goal.point:
             while curr.parent:
                 path.append(curr)
                 curr = curr.parent
@@ -61,27 +67,87 @@ def aStar(start, goal, maze, heuristic):
                     node.parent = curr
             else:
                 node.g = curr.g + curr.cost(node)
-                node.h = heuristic[node.value[0]][node.value[1]]
+                node.h = heuristic[node.point[0]][node.point[1]]
                 node.parent = curr
                 openset.append(node)
-    raise ValueError('Unsolvable')
+    print('Unsolvable')
+    return path
             
 def EuclidHeuristic(dim, maze):
     heuristic = [[0] * dim for i in range(dim)]
     for i in range(len(maze)):
         for j in range(len(maze[i])):
             heuristic[i][j] = math.sqrt((dim-1-i)**2+(dim-1-j)**2)
-    for row in heuristic:
-        print(' '.join([str(elem) for elem in row]))
+##    for row in heuristic:
+##        print(' '.join([str(elem) for elem in row]))
     return heuristic
 
-maze = grid(6, 0.3)
-heuristic = EuclidHeuristic(6, maze)
-for x in range(len(maze)):
-        for y in range(len(maze[x])):
-            maze[x][y] = Node(maze[x][y],[x,y])
-path = aStar([0,0], [5,5], maze, heuristic)
-print (len(path) - 1)
-for node in path:
-    x, y = node.point
-    print (x, y)
+def ManhattanHeuristic(dim, maze):
+    heuristic = [[0] * dim for i in range(dim)]
+    for i in range(len(maze)):
+        for j in range(len(maze[i])): 
+            heuristic[i][j] = abs(dim-1-i)+abs(dim-1-j)
+##    for row in heuristic:
+##        print(' '.join([str(elem) for elem in row]))
+    return heuristic
+
+def ChebyshevHeuristic(dim, maze):
+    heuristic = [[0] * dim for i in range(dim)]
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            heuristic[i][j] = max(abs(dim-1-i),abs(dim-1-j))
+##    for row in heuristic:
+##        print(' '.join([str(elem) for elem in row]))
+    return heuristic
+
+def part1():
+    table = []
+    dim = 101
+    p = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+    for i in p:
+        row = []
+        for j in range(50):
+            maze = grid(dim, i)
+            heuristic = EuclidHeuristic(dim, maze)
+            for x in range(len(maze)):
+                for y in range(len(maze[x])):
+                    maze[x][y] = Node(maze[x][y],[x,y], None, 0, heuristic[x][y])
+            path = aStar(maze[0][0], maze[dim-1][dim-1], maze, heuristic)
+            if path:
+                row.append(1)
+            else:
+                row.append(0)
+        table.append(row)
+    for row in table:
+        print(' '.join([str(elem) for elem in row]))
+
+def part2():
+    table = []
+    dim = 101
+    p = 0.25
+    for i in range(3):
+        row = []
+        for j in range(50):
+            maze = grid(dim, p)
+            if i is 0:
+                heuristic = EuclidHeuristic(dim, maze)
+            elif i is 1:
+                heuristic = ManhattanHeuristic(dim, maze)
+            elif i is 2:
+                heuristic = ChebyshevHeuristic(dim, maze)
+            for x in range(len(maze)):
+                for y in range(len(maze[x])):
+                    maze[x][y] = Node(maze[x][y],[x,y], None, 0, heuristic[x][y])
+            start_time = time.time()
+            path = aStar(maze[0][0], maze[dim-1][dim-1], maze, heuristic)
+            runtime = round(time.time() - start_time, 2)
+            if path:
+                row.append(runtime)
+        table.append(row)
+    for row in table:
+        print(sum(row)/len(row))
+part1()
+part2()
+
+
+
